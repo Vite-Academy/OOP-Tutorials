27bec0a4cfc0fe7abedd8b53719f0e9c7c81d7d2
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-08-16 21:07:45 +0500

    done

--- python_oop/product.py
+++ python_oop/product.py
@@ -1,16 +1,28 @@
+# https://pynative.com/
 class Product:
     """Creating a new product for me"""
 
     # Class attribute
     goal = "Production of new products"
-    
-    def __init__(self, name, company_name:str) -> None:
+
+    def __init__(self, name:str, company_name:str) -> None:
         # Class attribute
         self._placeholder = "Product"
 
         self._name = name
         self._company_name = company_name
 
+    # @staticmethod
+    # def get_name(self):
+    #     return self._name # TypeError
+
+
+    # def get_name(self):
+    #     return self._name # Product name
+
 
 # print(Product._placeholder) # AttributeError
-print(Product.goal) # Production of new products
+# print(Product.goal) # Production of new products
+
+product = Product("Product name", "Company name")
+print(product.get_name())
