cf74a2059513df07989af4af17d9561307e11b8c
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-08-16 20:07:52 +0500

    done

--- python_oop/processor.py
+++ python_oop/processor.py
@@ -6,9 +6,9 @@ class Processor:
     species = "Last generation"
 
     def __init__(self, company_name:str, panel_label:str, generation:str) -> None:
-        self.company_name = company_name
-        self.panel_label:str = panel_label
-        self.generation = generation
+        self._company_name = company_name
+        self._panel_label = panel_label
+        self._generation = generation
 
 
 intel_core_i3 = Processor("Intel","Core i3", "10th")
@@ -16,7 +16,17 @@ intel_core_i5 = Processor("Intel","Core i5", "11th")
 intel_core_i7 = Processor("Intel","Core i7", "12th")
 intel_core_i9 = Processor("Intel","Core i9", "13th")
 
-print(intel_core_i3)
-print(intel_core_i5)
-print(intel_core_i7)
-print(intel_core_i9)
+# print(intel_core_i3) # <__main__.Processor object at 0x7fe2b4e42410>
+# print(intel_core_i5) # <__main__.Processor object at 0x7fe2b4e42450>
+# print(intel_core_i7) # <__main__.Processor object at 0x7fe2b4e42490>
+# print(intel_core_i9) # <__main__.Processor object at 0x7fe2b4e424d0>
+
+
+# print(id(intel_core_i3)) # 140007546971216
+# print(id(intel_core_i5)) # 139655362782288
+# print(id(intel_core_i7)) # 140120257209488
+# print(id(intel_core_i9)) # 139642534503632
+
+
+# print(Processor.species) # Last generation
+# print(intel_core_i3.species) # Last generation
