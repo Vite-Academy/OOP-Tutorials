75d283902f4bbce657b6a2b8bee9ddffa151ec6d
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-08-17 08:26:27 +0500

    done

--- python_oop/product.py
+++ python_oop/product.py
@@ -11,18 +11,33 @@ class Product:
 
         self._name = name
         self._company_name = company_name
+    
+    def __str__(self) -> str:
+        return f"Product({self._name}, {self._company_name})"
+
 
     # @staticmethod
-    # def get_name(self):
+    # def get_name(self) -> str:
     #     return self._name # TypeError
 
 
-    # def get_name(self):
-    #     return self._name # Product name
+    def get_name(self) -> str:
+        return self._name # Product name
+
+    def get_company_name(self) -> str:
+        return self._company_name # Company name
 
 
 # print(Product._placeholder) # AttributeError
 # print(Product.goal) # Production of new products
 
+
 product = Product("Product name", "Company name")
-print(product.get_name())
+# print(product.get_name())
+# print(product.get_company_name())
+
+
+# Without __str__ method
+# print(product) # <__main__.Product object at 0x7f84789a6490>
+# With __str__ method
+# print(product) # Product(Product name, Company name)
